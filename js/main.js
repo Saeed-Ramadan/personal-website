c8bb7904e4c6728dfa0e843da30c184b249bc633
Eng:Saeed-Ramadan <83456924+Saeed-Ramadan@users.noreply.github.com>
2022-02-06 17:42:25 +0200

    This is personal website.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//scripting words
+var i = 0;
+var txt = "Front-end Web Developer"; /* The text */
+var speed = 100; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
